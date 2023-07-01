26ef4e1e58b01e8baf801a6af7c902df4dff7b72
“IshitaBharadwaj” <ishitabharadwaj04@gmail.com>
2023-07-01 17:02:13 +0530

    made some minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,11 @@
 import Login from "./components/Login";
-import Test from "./components/HomePage";
 import Register from "./components/Register";
 import LandingPage from "./components/LandingPage";
 import React from 'react';
 import {
   Routes,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
-import NavbarLogin from "./components/NavbarLogin";
-import HomeNavbar from "./components/Navbar";
 import HomePage from "./components/HomePage";
 
 
